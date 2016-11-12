7351034cabc96b43f856c118b47b86da9f4264bd
Tse <>
2016-11-12 17:04:37 +0800

    JS内置对象

--- .idea/code/xzq01/xzq01.js
+++ .idea/code/xzq01/xzq01.js
@@ -2,8 +2,102 @@
  * Created by Tse on 2016/11/12.
  */
 
-function add(a, b) {
-    return a + b;
-}
+//定义函数
+// function add(a, b) {
+//     return a + b;
+// }
+//
+// alert(add(11,22));
 
-alert(add(11,22));
+//定义类
+// var Person = {
+//     name:"xzq",
+//     age:20,
+//     paly:function () {
+//         return "玩";
+//     }
+// }
+//
+// alert(Person.paly())
+
+
+//定义类
+// function Person() {
+//
+// }
+// //原型量
+// Person.prototype = {
+//     name:"xzq",
+//     age:20,
+//     play:function () {
+//         return "玩";
+//     }
+// }
+// var p = new Person();
+// document.write(p.play());
+
+
+//闭包
+(function () {
+    function Person(name) {
+        this.name = name;
+    }
+
+    Person.prototype.play = function () {
+        document.write("Person" + this.name);
+    }
+
+    window.Person = Person;
+}());
+
+(function () {
+
+    function Student(name) {
+        this.name = name;
+    }
+
+    Student.prototype = new Person("dssd");
+    var superPlay = Student.prototype.play();
+    Student.prototype.play = function () {
+         //superPlay.call(this)
+        document.write("Student" + this.name);
+    }
+    window.Student = Student;
+}());
+
+var stu = new Student("xzq")
+stu.play()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
